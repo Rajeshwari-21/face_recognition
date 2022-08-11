5e670011af52a5ad6ca395e5a650d8bd2be63ad3
Rajeshwari-21 <91792392+Rajeshwari-21@users.noreply.github.com>
2022-08-10 22:12:28 -0700

    Add files via upload

---
+++ face_detector.py
@@ -0,0 +1,34 @@
+import cv2
+
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#img =  cv2.imread('face1.jpg')
+webcam = cv2.VideoCapture(0)
+
+while True:
+    successful_frame_read , frame = webcam.read()
+        
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    for (x, y, w, h) in face_coordinates :
+       cv2.rectangle(frame, (x,y) , (x+w, y+h), (256,0,0), 2)
+    
+    cv2.imshow('rajeshwari face detector', frame)
+    key = cv2.waitKey(1)
+    
+    if key== 81 or key==113 :
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+print("done")
